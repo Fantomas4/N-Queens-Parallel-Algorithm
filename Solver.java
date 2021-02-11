9822400c4a00e03c8b64d5ff7339e8dc18eb84f5
Stefanos Karamperas <stef1997s@gmail.com>
2021-02-11 07:08:41 +0200

    Fixed a bug that caused the CLI results grid to be printed in a wrong format.

--- Solver.java
+++ Solver.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solver {
-    int grid_size; // The size of the grid to be used (number "n" of the n-queens problem).
+    int gridSize; // The size of the grid to be used (number "n" of the n-queens problem).
     ArrayList<Integer[]> results = new ArrayList<>(); // Used to store the solutions found for the given n-queens problem.
     int threadsNumber; // The number of threads that should be used in finding a solution to the n-queens problem.
     Thread[] threads; // A list containing all the threads used in the problem solving procedure.
     private final Object lock = new Object(); // A lock used to synchronize access between threads to the results ArrayList.
 
-    public Solver(int grid_size) {
-        this.grid_size = grid_size;
-        this.threadsNumber = grid_size;
+    public Solver(int gridSize) {
+        this.gridSize = gridSize;
+        this.threadsNumber = gridSize;
         this.threads = new Thread[threadsNumber];
 
     }
@@ -19,7 +19,7 @@ public class Solver {
         for (int i = 0; i < threadsNumber; i++) {
             final int row = i;
             this.threads[i] = new Thread(() -> {
-                Integer[] rows = new Integer[this.grid_size];
+                Integer[] rows = new Integer[this.gridSize];
                 rows[0] = row;
                 solveQueens(1, rows);
             });
@@ -36,14 +36,14 @@ public class Solver {
     }
 
     public void solveQueens(int col, Integer[] rows) {
-        if (col == this.grid_size) {
+        if (col == this.gridSize) {
             // Found a complete n-queen solution
             synchronized(this.lock) {
                 this.results.add(rows.clone());
             }
         } else {
             // Continue looking for a solution recursively
-            for (int row = 0; row < this.grid_size; row++) {
+            for (int row = 0; row < this.gridSize; row++) {
                 if (checkValidity(rows, col, row)) {
                     rows[col] = row; // Place queen on the grid
                     solveQueens(col + 1, rows);
@@ -80,80 +80,115 @@ public class Solver {
         return true;
     }
 
+    private char[] getNumberDigitChars(int number) {
+        return String.valueOf(number).toCharArray();
+    }
+
     public void printSolutionGrid() {
-        System.out.printf("%n> There are %d solutions to the %d-Queens problem:%n%n", this.results.size(), this.grid_size);
+        System.out.printf("%n> There are %d solutions to the %d-Queens problem:%n%n", this.results.size(), this.gridSize);
+
+        // Calculate the length of the given grid size number
+        int gridSizeNumLen = String.valueOf(this.gridSize - 1).length();
 
         // The size for the grid used to print the results,
         // taking the additional space needed for border characters
         // into account.
-        int printGridSize = this.grid_size + 3;
+        int printGridSize = this.gridSize + gridSizeNumLen + 2;
 
-        int solCounter = 0; // Counter used to keep track of the solution
-        // currently being printed
+        int solCounter = 0; // Counter used to keep track of the solution currently being printed
 
         // Print a grid containing the queens positions
         // for each n-queens solution found
         for (Integer[] solution : this.results) {
             solCounter++;
-            System.out.printf(" Solution %d:%n", solCounter);
+            System.out.printf(" Solution %d%n", solCounter);
+            System.out.print(" Queens placed at:");
+            // Print the queen placement coordinates of the current solution
+            for (int q = 0; q < solution.length; q++) {
+                System.out.printf(" [%d, %d]", q, solution[q]);
+                if (q != solution.length - 1) {
+                    System.out.print(",");
+                }
+            }
+            System.out.print("\n\n");
 
             String[][] grid = new String[printGridSize][printGridSize];
 
-            // Initialize all grid positions with ""
+            // Initialize all grid positions with " "
             for (int i = 0; i < printGridSize; i++) {
                 for (int j = 0; j < printGridSize; j++) {
-                    grid[i][j] = "";
+                    grid[i][j] = " ";
                 }
             }
 
             // Set reference numbers at the left border of the grid
-            for (int i = 1; i < printGridSize - 2; i++) {
-                grid[i][0] = Integer.toString(this.grid_size - i);
+            for (int i = 1; i <= this.gridSize; i++) {
+                int indexNumber = this.gridSize - i;
+                char[] digitChars = getNumberDigitChars(indexNumber);
+                int k = digitChars.length - 1; // digitChars index counter
+
+                for (int j = gridSizeNumLen - 1; j >= 0; j--) {
+                    if (k >= 0) {
+                        grid[i][j] = Character.toString(digitChars[k]);
+                        k--;
+                    } else {
+                        grid[i][j] = " ";
+                    }
+                }
             }
 
             // Set reference numbers at the bottom border of the grid
-            grid[printGridSize - 1][0] = " ";
-            grid[printGridSize - 1][1] = " ";
-            for (int j = 2; j < printGridSize - 1; j++) {
-                grid[printGridSize - 1][j] = String.format(" %d ", j - 2);
-            }
+            for (int j = gridSizeNumLen + 1; j < printGridSize - 1; j++) {
+                int indexNumber = j - gridSizeNumLen - 1;
+                char[] digitChars = getNumberDigitChars(indexNumber);
+                int k = 0; // digitChars index counter
 
-            // Initialize all possible queen positions with .
-            for (int i = 1; i < printGridSize - 1; i++) {
-                for (int j = 2; j < printGridSize; j++) {
-                    grid[i][j] = " . ";
+                for (int i = printGridSize - gridSizeNumLen; i < printGridSize ; i++) {
+                    if (k < digitChars.length) {
+                        grid[i][j] = String.format(" %c ", digitChars[k]);
+                        k++;
+                    } else {
+                        grid[i][j] = "   ";
+                    }
                 }
             }
 
             // Set the + symbol at the grid corners
-            grid[0][1] = " +";
-            grid[0][printGridSize - 1] = "+";
-            grid[printGridSize - 2][1] = " +";
-            grid[printGridSize - 2][printGridSize - 1] = "+";
+            grid[0][gridSizeNumLen] = "+";                                     // top left corner
+            grid[0][printGridSize - 1] = "+";                                  // top right corner
+            grid[printGridSize - gridSizeNumLen - 1][gridSizeNumLen] = "+";    // bottom left corner
+            grid[printGridSize - gridSizeNumLen - 1][printGridSize - 1] = "+"; // bottom right corner
 
             // Set the - symbol at the top horizontal grid border
-            for (int j = 2; j < printGridSize - 1; j++) {
+            for (int j = gridSizeNumLen + 1; j < printGridSize - 1; j++) {
                 grid[0][j] = " - ";
             }
 
             // Set the - symbol at the bottom horizontal grid border
-            for (int j = 2; j < printGridSize - 1; j++) {
-                grid[printGridSize - 2][j] = " - ";
+            for (int j = gridSizeNumLen + 1; j < printGridSize - 1; j++) {
+                grid[printGridSize - gridSizeNumLen - 1][j] = " - ";
             }
 
             // Set the | symbol at the left vertical grid border
-            for (int i = 1; i < printGridSize - 2; i++) {
-                grid[i][1] = "|";
+            for (int i = 1; i < printGridSize - gridSizeNumLen - 1; i++) {
+                grid[i][gridSizeNumLen] = "|";
             }
 
             // Set the | symbol at the right vertical grid border
-            for (int i = 1; i < printGridSize - 2; i++) {
+            for (int i = 1; i < printGridSize - gridSizeNumLen - 1; i++) {
                 grid[i][printGridSize - 1] = "|";
             }
 
+            // Initialize all possible queen positions with .
+            for (int i = 1; i <= this.gridSize; i++) {
+                for (int j = gridSizeNumLen + 1; j < printGridSize - 1; j++) {
+                    grid[i][j] = " . ";
+                }
+            }
+
             // Add the queens positions to the grid
-            for (int j = 0; j < solution.length; j++) {
-                grid[solution[j] + 1][j + 2] = " X ";
+            for (int q = 0; q < solution.length; q++) {
+                grid[this.gridSize - q][gridSizeNumLen + 1 + solution[q]] = " X ";
             }
 
             // Print the generated grid
@@ -164,7 +199,7 @@ public class Solver {
                 System.out.println();
             }
 
-            System.out.print("\n\n");
+            System.out.print("\n\n\n");
         }
     }
 
